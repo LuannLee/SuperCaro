1fe4d3b4da3ee6345c4a7584397b3070cc657593
luanlee <luanlee2804@gmail.com>
2021-05-16 09:42:59 +0700

    Initialize Caro 2021

---
+++ src/app/main-layout/components/match-dual/match-dual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-match-dual',
+  templateUrl: './match-dual.component.html',
+  styleUrls: ['./match-dual.component.scss']
+})
+export class MatchDualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
